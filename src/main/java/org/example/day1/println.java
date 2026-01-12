315cfac204d6d4dc3328303c194044f5ee5463d4
Student-Tejas07 <tejas.ingale2005@gmail.com>
2026-01-12 10:57:41 +0530

    1st println

---
+++ src/main/java/org/example/day1/println.java
@@ -0,0 +1,9 @@
+package org.example.day1;
+
+public class println {
+    public static void main(String[] args) {
+        System.out.println( "Java" );
+        System.out.println( "Logic" );
+        System.out.println( "Building" );
+    }
+}
