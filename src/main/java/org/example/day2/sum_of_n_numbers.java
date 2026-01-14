827f2312fc8af9df477732e66bd7f04da822bce7
Student-Tejas07 <tejas.ingale2005@gmail.com>
2026-01-14 18:51:27 +0530

    sum_of_numbers

---
+++ src/main/java/org/example/day2/sum_of_n_numbers.java
@@ -0,0 +1,16 @@
+package org.example.day2;
+import java.util.Scanner;
+
+public class sum_of_n_numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum =0;
+        for (int i = 0; i<=n ; i++){
+        sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
