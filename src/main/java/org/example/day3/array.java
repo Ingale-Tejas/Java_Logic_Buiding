482de72abf6e114ac35aad6686954e31ff146dae
Student-Tejas07 <tejas.ingale2005@gmail.com>
2026-01-19 09:27:03 +0530

    Array_elements

---
+++ src/main/java/org/example/day3/array.java
@@ -0,0 +1,22 @@
+package org.example.day3;
+import java.util.Scanner;
+public class array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int n =  sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+
+        for(int i = 0; i<n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array elements are: ");
+
+        for(int i=0; i < n; i++){
+            System.out.print(arr[i] + " " );
+        }
+    }
+}
